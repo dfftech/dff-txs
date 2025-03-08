6dab2c142666849f4840f5f609e6638f8ceb0905
prasad <prasad@own.cc>
2025-03-09 00:02:19 +0530

    update the reactupdate

--- test/tx-text.tsx
+++ test/tx-text.tsx
@@ -1,20 +1,25 @@
-    const [auth, setAuth] = useState<AuthType>({} as AuthType);
-    const { handleSubmit, control, reset, formState: { errors } } = useForm<AuthType>({} as AuthType);
-    const onDefaultAuth = () => {
-        setAuth({} as AuthType);
+    const [otpVefify, setOtpVefify] = React.useState<OtpVefifyType>({} as OtpVefifyType);
+    const { handleSubmit, control, reset, formState: { errors } } = useForm<OtpVefifyType>({});
+    React.useEffect(() => {
+        onDefaultOtpVefify();
+        onResetOtpVefify();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+    const onDefaultOtpVefify = () => {
+        setOtpVefify({} as OtpVefifyType);
     };
-    const onResetAuth = () => {
-        reset({ ...auth });
+    const onResetOtpVefify = () => {
+        reset({ ...otpVefify });
     };
-    const onSubmitAuth = async (data: AuthType) => {
-        console.log("data auth : ", data);
+    const onSubmitOtpVefify = async (data: OtpVefifyType) => {
+        console.log('data otpVefify : ', data);
         RouterChange(AppRouter.INDEX);
     };
     const onCancel= () => {
         RouterChange(AppRouter.INDEX);
     };
     const ComponentSubmit = () => (
-        <TypeButton name="Send" label="Submit" onPress={handleSubmit(onSubmitAuth)} />
+        <TypeButton name="SendHorizontal" label="Submit" onPress={handleSubmit(onSubmitOtpVefify)} />
     );
     const ComponentCancel = () => (
         <TypeButton name="CircleX" label="Cancel" action="negative" onPress={onCancel} />
